29acba5811263ce2098097cdf44d4af109a5a1c1
Ilya Petrov <ilya.muromec@gmail.com>
2011-11-29 22:41:15 +0200

    add setup.py, change files location

--- param.js
+++
@@ -1,7 +0,0 @@
-var param = {
-    "t": "nyaaaa",
-    "myname": 'yoba',
-    "names": [ ],
-    "my_num": 3,
-    "my_list": [1,2,3],
-}
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name = "jscrap",
+    version = "0.2",
+    packages = ['jscrap'],
+    package_data = {'jscrap': ['js/*']}
+)
